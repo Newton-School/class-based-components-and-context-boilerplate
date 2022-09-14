3764d8b18de779ddd540230ef8f84cdd2fd80a11
mrstark14 <sanjeet_m@ch.iitr.ac.in>
2022-09-14 17:07:44 +0530

    done

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../styles/App.css';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+
+export default App;
